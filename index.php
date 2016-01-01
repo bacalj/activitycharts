327a1d687852d7ebcc164a85036ed6de3e594b24
Joe Bacal <jebacal@smith.edu>
2015-12-31 22:42:14 -0500

    temporarily commenting out the main script so I can rebuild as a class in another file

--- index.php
+++ index.php
@@ -12,64 +12,63 @@
 require(dirname(__FILE__).'/../../config.php');
 require_once($CFG->libdir.'/adminlib.php');
 
-
 admin_externalpage_setup('reportactivitycharts', '', null, '', array('pagelayout'=>'report'));
 echo $OUTPUT->header();
 echo $OUTPUT->heading(get_string('activitycharts', 'report_activitycharts'));
 
 include('moodlehighchart.php');
 
-$event_to_count = $_GET['event-dropdown'] == null ? '\core\event\user_loggedin' : $_GET['event-dropdown'];
-$start_date = $_GET['event-dropdown'] == null ? '2015-09-01' : $_GET['startdate'];
-$end_date = $_GET['event-dropdown'] == null ? '2015-09-03' : $_GET['enddate'];
-
-global $DB;
-
-//get a list of all possible events
-$events_list = $DB->get_records_sql('SELECT DISTINCT eventname FROM {logstore_standard_log}');
-
-//get a period object for the timespan
-$timespan = new DatePeriod(
-     new DateTime($start_date),
-     new DateInterval('P1D'),
-     new DateTime($end_date)
-);
-
-//turn it into an array
-$days = iterator_to_array($timespan);
-
-//set up an empty array for our date list, then push dates into it
-$dates = array();
-foreach ($days as $day) {
-  $date_string = $day->format('Y-m-d');
-  array_push($dates, $date_string);
-}
-
-//set up an empty array for counts, then put counts into it
-$counts = array();
-
-foreach ($dates as $date){
-
-  $next_date = date('Y-m-d', strtotime($date .  "+1 day"));
-
-  $params = array(
-    'daystarts' => strtotime($date),
-    'dayends' => strtotime($next_date),
-    'whichevent' => $event_to_count
-  );
-
-  $sql =  "SELECT * FROM {logstore_standard_log}";
-  $sql .= " WHERE timecreated > :daystarts";
-  $sql .= " AND timecreated < :dayends";
-  $sql .= " AND eventname = :whichevent";
-
-  $all_for_day = $DB->get_records_sql($sql, $params);
-  array_push($counts, count($all_for_day));
-
-}
-
-//map the dates to the counts
-$dates_counts = array_combine($dates, $counts);
+// $event_to_count = $_GET['event-dropdown'] == null ? '\core\event\user_loggedin' : $_GET['event-dropdown'];
+// $start_date = $_GET['event-dropdown'] == null ? '2015-09-01' : $_GET['startdate'];
+// $end_date = $_GET['event-dropdown'] == null ? '2015-09-03' : $_GET['enddate'];
+//
+// global $DB;
+//
+// //get a list of all possible events
+// $events_list = $DB->get_records_sql('SELECT DISTINCT eventname FROM {logstore_standard_log}');
+//
+// //get a period object for the timespan
+// $timespan = new DatePeriod(
+//      new DateTime($start_date),
+//      new DateInterval('P1D'),
+//      new DateTime($end_date)
+// );
+//
+// //turn it into an array
+// $days = iterator_to_array($timespan);
+//
+// //set up an empty array for our date list, then push dates into it
+// $dates = array();
+// foreach ($days as $day) {
+//   $date_string = $day->format('Y-m-d');
+//   array_push($dates, $date_string);
+// }
+//
+// //set up an empty array for counts, then put counts into it
+// $counts = array();
+//
+// foreach ($dates as $date){
+//
+//   $next_date = date('Y-m-d', strtotime($date .  "+1 day"));
+//
+//   $params = array(
+//     'daystarts' => strtotime($date),
+//     'dayends' => strtotime($next_date),
+//     'whichevent' => $event_to_count
+//   );
+//
+//   $sql =  "SELECT * FROM {logstore_standard_log}";
+//   $sql .= " WHERE timecreated > :daystarts";
+//   $sql .= " AND timecreated < :dayends";
+//   $sql .= " AND eventname = :whichevent";
+//
+//   $all_for_day = $DB->get_records_sql($sql, $params);
+//   array_push($counts, count($all_for_day));
+//
+// }
+//
+// //map the dates to the counts
+// $dates_counts = array_combine($dates, $counts);
 
 ?>
 
